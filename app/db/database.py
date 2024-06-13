14f5804262821e7d0ae8eda3a46df8d82f688949
juansaga <juansaga00@gmail.com>
2024-06-12 20:34:17 -0500

    Rama main actualizada para conectarse a bd postgres con los cambios del front en consultar clientes

--- app/db/database.py
+++ app/db/database.py
@@ -12,19 +12,14 @@ load_dotenv(dotenv_path=env_path / 'database.env')
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Crear el motor de la base de datos utilizando SQLAlchemy
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
-# Metadatos de SQLAlchemy para la recopilación de modelos
 metadata = MetaData()
 
-# Crear una instancia de Database de 'databases' para interacciones asincrónicas
 database = Database(DATABASE_URL)
 
-# Configurar una sesión de SQLAlchemy para operaciones ORM
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Función para obtener una sesión de base de datos, útil para dependencias
 def get_db():
     db = SessionLocal()
     try:
