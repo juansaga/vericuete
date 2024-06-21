c32bb51d565cc1f59a5ba3fb14e91dc50f4183ad
sergio davila <sergiodavila94@gmail.com>
2024-06-20 23:01:01 -0500

    Se agrega buscador de orden, y botón de buscar orden. Además se crea conexión con el Js y los elementos necesarios

--- front/js/consultar_orden.js
+++ front/js/consultar_orden.js
@@ -1,22 +1,14 @@
-async function loadOrders() {
-    // Realiza la petición GET para obtener todas las órdenes
-    const response = await fetch('http://127.0.0.1:8000/orden/', { method: 'GET' });
-    const orders = await response.json();
+document.getElementById('order_search_form').addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-    const ordersList = document.getElementById('orders');
-    ordersList.innerHTML = ''; // Limpiar la lista actual
-
-    // Itera sobre cada orden y la añade a la lista en el DOM
-    orders.forEach(order => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `Orden ID: ${order.ordenid}, Cliente ID: ${order.clienteid}, Fecha: ${order.fechaorden}, Cantidad de Prendas: ${order.totalprendas}, Estado: ${order.estado}, Pago: ${order.pago}`;
-        // Añade más detalles de la orden si lo deseas
-
-        ordersList.appendChild(listItem);
+    // Extracción de parámetros
+    const orden = document.getElementById('order_search_input').value;
+    const response = await fetch(`http://127.0.0.1:8000/ordenes/?orden_id=${orden}`, {
+        method: 'GET'
     });
-}
 
-// Llamar a loadOrders al cargar la página para mostrar la lista de órdenes existentes
-document.addEventListener('DOMContentLoaded', () => {
-    loadOrders();
-});
+    const ordenes = await response.json();
+
+    const detalle_ordenes = [ordenes.ordenid, ordenes.clienteid, ordenes.fechaorden, ordenes.totalprendas,ordenes.estado, ordenes.pago]
+})
+
